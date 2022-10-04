baa7f966682632bc7a4038951354aeb685c8eaf8
eveugla <pltevaz@mail.ru>
2022-10-04 18:48:40 +0300

    Задача 8

--- Homework1/Task4/Program.cs
+++ Homework1/Task4/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+int N;
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine(), out N);
+
+int i = 1;
+while (i <= N)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write($"{i}, ");
+        
+    }
+    i++;
+}
