6077e4f6f5a2be51444967b99892c5108588ba5b
eveugla <pltevaz@mail.ru>
2022-10-04 22:25:31 +0300

    added variables

--- Homework1/Task1/Program.cs
+++ Homework1/Task1/Program.cs
@@ -21,3 +21,22 @@ else
 {
     Console.WriteLine($"max = {b}");
 }
+
+/*
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3 */
+/*
+int a, b;
+Console.Write($"Введите первое число: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"Введите второе число: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+if (a > b)
+    Console.Write($"Наибольшее число = {a}");
+else
+    Console.Write($"Наибольшее число = {b}");
+*/
--- Homework1/Task2/Program.cs
+++ Homework1/Task2/Program.cs
@@ -21,3 +21,24 @@ if (b > max) max = b;
 if (c > max) max = c;
 
 Console.WriteLine(max);
+
+/*
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22 */
+/*
+int a, b, c;
+a = new Random().Next(10);
+b = new Random().Next(10);
+c = new Random().Next(10);
+Console.Write($"{a} {b} {c} ");
+
+int max = a;
+if (b > max)
+    max = b;
+if (c > max)
+    max = c;
+Console.Write($"\nМаксимальное число = {max}");
+*/
