1ae8254a0c02d2169ac57c0a2c8bb06c83310002
eveugla <pltevaz@mail.ru>
2022-10-04 18:45:33 +0300

    Задача 6

--- Homework1/Task3/Program.cs
+++ Homework1/Task3/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+
+int a;
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine(), out a);
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
