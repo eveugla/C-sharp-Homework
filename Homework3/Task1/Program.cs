c0764e8592ca76d83027bb01c16076f9767a26ac
eveugla <pltevaz@mail.ru>
2022-10-08 19:57:37 +0300

    added vars

--- Homework3/Task1/Program.cs
+++ Homework3/Task1/Program.cs
@@ -20,3 +20,31 @@ else
 {
     Console.WriteLine($"Число {N} - НЕ палиндром!");
 }
+
+/*
+/*    Задача 19
+Напишите программу, которая принимает на вход пятизначное 
+число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да    */
+/*
+int a;
+int.TryParse(Console.ReadLine()!, out a);
+
+string str = a.ToString();
+
+if (str[0] == str[4] && str[1] == str[3])
+    Console.Write($"Число является палиндромом");
+else
+    Console.Write($"Число не является палиндромом");
+*/
+/*
+int a;
+int.TryParse(Console.ReadLine()!, out a);
+
+if (a % 10 == a / 10000 && a / 10 % 10 == a / 1000 % 10)
+    Console.Write($"Число является палиндромом");
+else
+    Console.Write($"Число не является палиндромом");
+*/
