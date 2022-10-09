3f39c2b3a5aa582f3fee23f4a978da4a7996b0fa
eveugla <pltevaz@mail.ru>
2022-10-09 17:35:17 +0300

    Задача 25

--- Homework4/Task1/Program.cs
+++ Homework4/Task1/Program.cs
@@ -3,3 +3,17 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16
 */
+int a; int b; 
+Console.Write($"Введите число A: ");
+int.TryParse(Console.ReadLine()!, out a);
+Console.Write($"Введите число B: ");
+int.TryParse(Console.ReadLine()!, out b);
+
+int Pow(int _a, int _b)
+{
+    int power = 1;
+    for (int i = 1; i <= _b; i++)
+        power = power * a;
+    return power;
+}
+Console.Write($"Число {a} в степени {b} = {Pow(a, b)}");
