95144440211eff7790693fdeb07eae7c1e1b7efd
eveugla <pltevaz@mail.ru>
2022-10-09 20:36:59 +0300

    Задача 29

--- Homework4/Task3/Program.cs
+++ Homework4/Task3/Program.cs
@@ -3,3 +3,40 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
+void FillArray(int[] array)
+{
+    int i = 0;
+    while (i < 8)
+    {
+        Console.Write($"Введите число {i}: ");
+         array[i] =  int.Parse(Console.ReadLine());
+        i++;
+    }
+}
+
+void PrintArray(int[] amount)
+{
+    int pos = 0;
+    while (pos < 8)
+    {
+        if (pos == 0)
+        {
+            Console.Write("[" + amount[pos] + ", ");
+        }
+        else if (pos == 7)
+        {
+            Console.Write(amount[pos] + "]");
+        }
+        else
+        {
+            Console.Write(amount[pos] + ", ");
+        }
+        pos++;
+    }
+}
+
+int[] array = new int [8];
+
+
+FillArray(array);
+PrintArray(array);
