ada7759d757f1006ba4e26025f654f0a17852409
eveugla <pltevaz@mail.ru>
2022-10-11 18:07:04 +0300

    Задача 36

--- Homework5/Task2/Program.cs
+++ Homework5/Task2/Program.cs
@@ -3,3 +3,35 @@
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
 */
+int size;
+Console.WriteLine("Введите размер массива");
+int.TryParse(Console.ReadLine()!, out size);
+
+int[] n = new int[size];
+FillArray(n); 
+PrintArray(n);
+int sum = 0;
+
+for (int i = 0; i < n.Length; i++)
+if (n[i] % 2 != 0)
+    sum = sum + n[i];
+
+Console.WriteLine($"В массиве {n.Length} чисел, сумма нечётных - {sum}");
+
+void FillArray(int[] _n)
+{
+    for(int i = 0; i < _n.Length; i++)
+    {
+        _n[i] = new Random().Next(1,30);
+    }
+}
+void PrintArray(int[] _n)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < _n.Length; i++)
+    {
+        Console.Write(_n[i] + " ");
+    }
+    Console.Write("]");
+    Console.WriteLine();
+}
