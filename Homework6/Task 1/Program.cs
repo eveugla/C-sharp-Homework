60d54bff63eea6d362983ea09adcf032f59532f9
eveugla <pltevaz@mail.ru>
2022-10-12 14:41:23 +0300

    задача 41

--- Homework6/Task 1/Program.cs
+++ Homework6/Task 1/Program.cs
@@ -3,3 +3,36 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3
 */
+int size;
+Console.WriteLine("Введите количество чисел М: ");
+int.TryParse(Console.ReadLine()!, out size);
+
+int[] n = new int[size];
+FillArray(n); 
+PrintArray(n);
+int count = 0;
+
+for (int i = 0; i < n.Length; i++)
+if (n[i] > 0)
+count++;
+
+Console.WriteLine($"В массиве {n.Length} чисел, чисел больше нуля: {count}");
+
+void FillArray(int[] _n)
+{
+    for(int i = 0; i < _n.Length; i++)
+    {
+        int a;
+        Console.Write($"Введите элемент[{i}]: ");
+        int.TryParse(Console.ReadLine()!, out a);
+        _n[i] = a;
+    }
+}
+void PrintArray(int[] _n)
+{
+    for(int i = 0; i < _n.Length; i++)
+    {
+        Console.Write(_n[i] + " ");
+    }
+    Console.WriteLine();
+}
