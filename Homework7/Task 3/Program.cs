73db09291aeff1b5a4a99a7c8315ee326e694336
eveugla <pltevaz@mail.ru>
2022-10-14 18:25:11 +0300

    задача 52

--- Homework7/Task 3/Program.cs
+++ Homework7/Task 3/Program.cs
@@ -6,3 +6,52 @@
 8 4 2 4
 Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 */
+int m, n;
+
+Console.Write($"Введите количество строк n: ");
+int.TryParse(Console.ReadLine()!, out n);
+Console.Write($"Введите количество столбцов m: ");
+int.TryParse(Console.ReadLine()!, out m);
+
+int[,] array = FillArray(n, m);
+double[] sum = new double[m];
+PrintArray(array);
+
+int[,] FillArray(int n, int m)
+{
+    int[,] result = new int[n, m];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            result[i, j] = new Random().Next(-10, 10);
+        }
+    }
+    return result;
+}
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        sum[j] += array[i, j];
+    }
+}
+for (int j = 0; j < m; j++)
+{
+    Console.Write($"{(sum[j] / n), 1} ");
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            Console.Write($"{array[i, j]} ");
+
+        }
+        Console.WriteLine();
+    }
+}
