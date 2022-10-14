0d856b96989a7e35c4f6426cdb3850a6cbae43c4
eveugla <pltevaz@mail.ru>
2022-10-14 15:21:19 +0300

    задача 47

--- Homework7/Task 1/Program.cs
+++ Homework7/Task 1/Program.cs
@@ -6,3 +6,22 @@ m = 3, n = 4.
 1 -3,3 8 -9,9
 8 7,8 -7,1 9
 */
+int m, n;
+
+Console.Write($"Введите размерность m: ");
+int.TryParse(Console.ReadLine()!, out m);
+Console.Write($"Введите размерность n: ");
+int.TryParse(Console.ReadLine()!, out n);
+
+
+double[,] array = new double[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = Math.Round(new Random().NextDouble() * 10, 1);
+        Console.Write($"{array[i, j]} ");
+    }
+    Console.WriteLine();
+}
