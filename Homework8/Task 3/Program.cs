895489c9c2f003316a027e62671d64400ae20212
eveugla <pltevaz@mail.ru>
2022-10-15 11:00:59 +0300

    задача 58

--- Homework8/Task 3/Program.cs
+++ Homework8/Task 3/Program.cs
@@ -7,3 +7,66 @@
 18 20
 15 18
 */
+int m = ReadInt("Введите количество строк m: ");
+int n = ReadInt("Введите количество столбцов n: ");
+int[,] array = new int[m, n];
+int[,] array2 = new int[m, n];
+int[,] array3 = new int[m, n];
+
+FillArray(array);
+PrintArray(array);
+
+Console.WriteLine();
+
+FillArray(array2);
+PrintArray(array2);
+
+Console.WriteLine();
+
+if (array.GetLength(0) != array2.GetLength(1))
+{
+    Console.WriteLine("Не перемножаются");
+    return;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array2.GetLength(1); j++)
+    {
+        array3[i, j] = 0;
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            array3[i, j] += array[i, k] * array2[k, j];
+        }
+    }
+}
+
+PrintArray(array3);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
