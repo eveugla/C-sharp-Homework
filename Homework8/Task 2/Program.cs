08792a90a04098f22f7a4dbdecc8f1e188239e00
eveugla <pltevaz@mail.ru>
2022-10-15 10:43:50 +0300

    задача 56

--- Homework8/Task 2/Program.cs
+++ Homework8/Task 2/Program.cs
@@ -8,3 +8,53 @@
 5 2 6 7
 Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
 */
+int size;
+
+Console.Write($"Введите размер прямоугольного массива: ");
+int.TryParse(Console.ReadLine()!, out size);
+
+int[,] numbers = new int[size, size];
+FillArray(numbers);
+PrintArray(numbers);
+
+int sumOfMin = Int32.MaxValue;
+int index = 0;
+
+for (int i = 0; i < numbers.GetLength(0); i++)
+{
+    int sum = 0;
+    for (int j = 0; j < numbers.GetLength(1); j++)
+    {
+        sum = sum + numbers[i, j];        
+    }
+    if (sum < sumOfMin)
+    {
+        sumOfMin = sum;
+        index++;
+    }
+}
+
+Console.WriteLine("Строка с наименьшей суммой элементов: " + (index) + ". Сумма равна: " + (sumOfMin));
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
