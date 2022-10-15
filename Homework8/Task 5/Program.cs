d2b04899a0607e78ad7dc6793ab74d8449c20ab8
eveugla <pltevaz@mail.ru>
2022-10-15 11:16:24 +0300

    задача 62

--- Homework8/Task 5/Program.cs
+++ Homework8/Task 5/Program.cs
@@ -6,3 +6,37 @@
 11 16 15 06
 10 09 08 07
 */
+
+int[,] array = new int[4, 4];
+
+int n = 1;
+int i = 0;
+int j = 0;
+
+while (n <= 4 * 4)
+{
+    array[i, j] = n;
+    if (i <= j + 1 && i + j < 4 - 1)
+        ++j;
+    else if (i < j && i + j >= 4 - 1)
+        ++i;
+    else if (i >= j && i + j > 4 - 1)
+        --j;
+    else
+        --i;
+    ++n;
+}
+
+PrintArray(array);
+
+void PrintArray(int[,] sparray)
+{
+    for (int i = 0; i < sparray.GetLength(0); i++)
+    {
+        for (int j = 0; j < sparray.GetLength(1); j++)
+        {
+            Console.Write(sparray[i, j] + " ");
+        }
+        Console.WriteLine("");
+    }
+}
